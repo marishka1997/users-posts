0323a51c59d1e345452af2aca7d98c7d176f9c34
Mariam Gasparian <m.gasparian11@gmail.com>
2023-08-12 23:12:18 +0400

    fix: remove UserPostsComponent for @NgModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { UserPostsComponent } from './core/modules/user-posts/user-posts.component';
 import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [
     AppComponent,
-    UserPostsComponent
   ],
   imports: [
     BrowserModule,
