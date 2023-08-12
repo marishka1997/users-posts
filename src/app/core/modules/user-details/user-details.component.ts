be01f55f4dbe80a9319a6c2748e6102f8aedfd68
Mariam Gasparian <m.gasparian11@gmail.com>
2023-08-12 22:59:48 +0400

    fix: implements displayUserDetails and getUserDetails methods in UserDetailsComponent

--- src/app/core/modules/user-details/user-details.component.ts
+++ src/app/core/modules/user-details/user-details.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { User } from '../../models/user.model';
+import { UserDetailsService } from '../../services/user-details.service';
+import { ActivatedRoute, Router } from '@angular/router';
 @Component({
   selector: 'app-user-details',
   templateUrl: './user-details.component.html',
   styleUrls: ['./user-details.component.scss']
 })
-export class UserDetailsComponent {
+export class UserDetailsComponent implements OnInit {
+  user!: User;
 
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private userDetails: UserDetailsService
+  ) {}
+
+  ngOnInit(): void {
+    this.userDetails
+      .getUserDetails(this.route.snapshot.params['id'])
+      .subscribe((User) => (this.user = User));
+  }
+
+  displayUserPosts() {
+    this.router.navigate(['posts'], { relativeTo: this.route });
+  }
 }
