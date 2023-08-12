0ff0ba6520e0e2bf9847b256096c559eb8a4416b
Mariam Gasparian <m.gasparian11@gmail.com>
2023-08-12 23:04:18 +0400

    fix: remove UserDetailsComponent from @NgModule in app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { UserDetailsComponent } from './core/modules/user-details/user-details.component';
 import { UserPostsComponent } from './core/modules/user-posts/user-posts.component';
 import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [
     AppComponent,
-    UserDetailsComponent,
     UserPostsComponent
   ],
   imports: [
