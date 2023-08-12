70b5172f366a324bc92e0457e20ed9e5adfb6722
Mariam Gasparian <m.gasparian11@gmail.com>
2023-08-12 22:56:17 +0400

    feat: implements angular/material modules for UserDetailsModule

--- src/app/core/modules/user-details/user-details.module.ts
+++ src/app/core/modules/user-details/user-details.module.ts
@@ -1,14 +1,17 @@
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {UserDetailsRoutingModule} from './user-details-routing.module';
-import {UserDetailsComponent} from "./user-details.component";
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UserDetailsRoutingModule } from './user-details-routing.module';
+import { UserDetailsComponent } from './user-details.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [UserDetailsComponent],
   imports: [
     CommonModule,
     UserDetailsRoutingModule,
-  ]
+    MatCardModule,
+    MatButtonModule,
+  ],
 })
-export class UserDetailsModule {
-}
+export class UserDetailsModule {}
