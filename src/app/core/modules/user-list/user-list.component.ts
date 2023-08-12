e60ce35a8d4ad78d26dce4d8b77f144f35d272d0
Mariam Gasparian <m.gasparian11@gmail.com>
2023-08-12 22:33:31 +0400

    feat: implements viewUserDetails() method

--- src/app/core/modules/user-list/user-list.component.ts
+++ src/app/core/modules/user-list/user-list.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
-
+import { UserListService } from '../../services/user-list.service';
+import { User } from '../../models/user.model';
+import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
   styleUrls: ['./user-list.component.scss']
 })
-export class UserListComponent {
+export class UserListComponent implements OnInit {
+  users!: Observable<User[]>;
 
+  constructor(private userList: UserListService, private router: Router) {}
+
+  ngOnInit() {
+    this.users = this.userList.getUsers();
+  }
+
+  viewUserDetails(id: number) {
+    this.router.navigate([`users/${id}`]);
+  }
 }
